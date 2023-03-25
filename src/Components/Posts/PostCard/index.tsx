eca7eb7f2d88f5c45ff2c512169c2251c191fb6d
Erichev Egor <ErichevEA@lad24.ru>
2023-03-25 10:48:41 +0300

    добавить карточку постов

--- src/Components/Posts/PostCard/index.tsx
+++ src/Components/Posts/PostCard/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import styles from './PostCard.module.scss';
 
 export const PostCard = () => {
-  return <div>PostCard</div>;
+  return (
+    <div className={styles.cardWrapper}>
+      <span className={styles.label}>Post Label</span>
+      <p className={styles.content}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam asperiores aperiam eligendi
+        voluptates sit beatae, qui dolor. Asperiores, odit, porro impedit cum consectetur
+        dignissimos quam praesentium voluptatum, ipsam explicabo illum!
+      </p>
+    </div>
+  );
 };
