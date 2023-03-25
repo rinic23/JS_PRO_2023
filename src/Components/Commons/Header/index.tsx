afe3d6b6c837e3aa554faeb9fe40a54f76bf4edd
Erichev Egor <ErichevEA@lad24.ru>
2023-03-25 10:23:15 +0300

    Добавить scss

--- src/Components/Commons/Header/index.tsx
+++ src/Components/Commons/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Header.module.css';
+import styles from './Header.module.scss';
 
 export const Header = () => {
   return <header className={styles.header}>Header</header>;
--- src/Containers/PostsPage/index.tsx
+++ src/Containers/PostsPage/index.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { Footer } from '../../Components/Commons/Footer';
 import { Header } from '../../Components/Commons/Header';
 import { PostCard } from '../../Components/Posts/PostCard';
+import styles from './PostsPage.module.scss';
 
 export const PostsPage = () => {
   return (
-    <div>
+    <div className={styles.postPageWrapper}>
       <Header />
-      <span>PostsPage</span>
-      <PostCard />
+      <div className={styles.postList}>
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </div>
       <Footer />
     </div>
   );
