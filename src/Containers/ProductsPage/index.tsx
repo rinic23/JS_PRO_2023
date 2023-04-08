32aaab04709481bb06adb7968044ce4d732878ed
Egor Erichev <erichev1988@mail.ru>
2023-04-08 11:46:46 +0300

    add product view

--- src/Containers/ProductsPage/index.tsx
+++ src/Containers/ProductsPage/index.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { ProductCard } from '../../Components/Products/ProductCard';
 import styles from './ProductsPage.module.scss';
+import { useSelector } from 'react-redux';
+import { getProducts } from '../../store/products/selectors';
 
 export const ProductsPage = () => {
   const [count, setCount] = useState(0);
+  const products = useSelector(getProducts);
 
   const handleButtonClick = () => {
     setCount(count + 1);
@@ -14,6 +17,15 @@ export const ProductsPage = () => {
       <button onClick={handleButtonClick}>{count}</button>
       <ProductCard productName={`Apple ${count}`} />
       <ProductCard productName="Orange" />
+      <div>
+        <span>Корзина</span>
+        {products.map((product, index) => (
+          <div key={index}>
+            <span>{`Название товара -  ${product.name}`}</span>
+            <span>{`Колличество товара ${product.count}`}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
