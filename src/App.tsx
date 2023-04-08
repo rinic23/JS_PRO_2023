fc28127838c3530a88f98444dc5f518074b0b0b0
Egor Erichev <erichev1988@mail.ru>
2023-04-08 12:00:05 +0300

    add checkAuth

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,47 @@ import { PostsPage } from './Containers/PostsPage';
 import { ProductsPage } from './Containers/ProductsPage';
 import { routes } from './utils/constants/routes';
 import { AuthPage } from './Containers/AuthPage';
+import { CheckAuth } from './Components/Commons/CheckAuth';
 
 function App() {
   return (
     <div className={styles.wrapper}>
       <Layout>
         <Routes>
-          <Route path={routes.MAIN} element={<MainPage />} />
-          <Route path={routes.PRODUCTS} element={<ProductsPage />} />
+          <Route
+            path={routes.MAIN}
+            element={
+              <CheckAuth>
+                <MainPage />
+              </CheckAuth>
+            }
+          />
+          <Route
+            path={routes.PRODUCTS}
+            element={
+              <CheckAuth>
+                <ProductsPage />
+              </CheckAuth>
+            }
+          />
           <Route path={routes.AUTH} element={<AuthPage />} />
           <Route path={routes.POSTS}>
-            <Route element={<PostsPage />} index />
-            <Route path=":id" element={<PostPage />} />
+            <Route
+              element={
+                <CheckAuth>
+                  <PostsPage />
+                </CheckAuth>
+              }
+              index
+            />
+            <Route
+              path=":id"
+              element={
+                <CheckAuth>
+                  <PostPage />
+                </CheckAuth>
+              }
+            />
           </Route>
           <Route />
         </Routes>
