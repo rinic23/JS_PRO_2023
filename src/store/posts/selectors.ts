8e00c443c70167e0bc5e0a46b3a13f01409016a6
Egor Erichev <erichev1988@mail.ru>
2023-04-15 11:26:34 +0300

    fix

--- src/store/posts/selectors.ts
+++ src/store/posts/selectors.ts
@@ -7,3 +7,20 @@ export const getPostListSelector = createSelector(getPostsState, ({ posts }) => 
 export const getPostListIsError = createSelector(getPostsState, ({ posts }) => posts.isError);
 export const getPostListIsSuccess = createSelector(getPostsState, ({ posts }) => posts.isSuccess);
 export const getPostListIsLoading = createSelector(getPostsState, ({ posts }) => posts.isLoading);
+
+export const getPostByIdSelector = createSelector(
+  getPostsState,
+  ({ currentPost }) => currentPost.data,
+);
+export const getPostByIdIsError = createSelector(
+  getPostsState,
+  ({ currentPost }) => currentPost.isError,
+);
+export const getPostByIdIsSuccess = createSelector(
+  getPostsState,
+  ({ currentPost }) => currentPost.isSuccess,
+);
+export const getPostByIdIsLoading = createSelector(
+  getPostsState,
+  ({ currentPost }) => currentPost.isLoading,
+);
