7d233102a97cd5e183964370461074144e308230
Egor Erichev <erichev1988@mail.ru>
2023-04-15 11:06:31 +0300

    fix

--- src/store/posts/effects.ts
+++ src/store/posts/effects.ts
@@ -3,7 +3,7 @@ import { networkInstance } from '../../utils/network';
 
 export const getPostList = createAsyncThunk('/post/list', async (_, { rejectWithValue }) => {
   try {
-    const response = await networkInstance.get('/postsssss');
+    const response = await networkInstance.get('/posts');
     console.log(response);
     return response.data;
   } catch (e) {
