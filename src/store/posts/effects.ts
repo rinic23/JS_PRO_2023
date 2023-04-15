80dc4c01442e79873f06dc101a3892ba5480139f
Egor Erichev <erichev1988@mail.ru>
2023-04-15 11:18:28 +0300

    fix

--- src/store/posts/effects.ts
+++ src/store/posts/effects.ts
@@ -10,3 +10,16 @@ export const getPostList = createAsyncThunk('/post/list', async (_, { rejectWith
     return rejectWithValue(e);
   }
 });
+
+export const getPostById = createAsyncThunk(
+  '/post/byId',
+  async (id: number, { rejectWithValue }) => {
+    try {
+      const response = await networkInstance.get(`/posts/${id}`);
+      console.log(response);
+      return response.data;
+    } catch (e) {
+      return rejectWithValue(e);
+    }
+  },
+);
