00ac6e79a32d9ace410c82a8215bda616c40f614
Egor Erichev <erichev1988@mail.ru>
2023-04-15 10:06:37 +0300

    fix

--- src/store/posts/index.ts
+++ src/store/posts/index.ts
@@ -1,13 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { getPostList } from './effects';
 
-const initialState = {
-  data: [],
+interface IPostsState {
+  posts: {
+    data: any[];
+    isSuccess: boolean;
+    isLoading: boolean;
+    isError: boolean;
+  };
+}
+
+const initialState: IPostsState = {
+  posts: {
+    data: [],
+    isSuccess: false,
+    isLoading: false,
+    isError: false,
+  },
 };
 
 const slice = createSlice({
   name: 'posts',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getPostList.pending, (state) => {
+      return state;
+    });
+  },
 });
 
 export const { reducer: postsDataReducer, name: postsDataNameReducer } = slice;
