acdc280ef48b26172bb1d9196f1cadc76008f477
Egor Erichev <erichev1988@mail.ru>
2023-04-15 09:57:55 +0300

    add async action

---
+++ src/store/posts/effects.ts
@@ -0,0 +1,12 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { networkInstance } from '../../utils/network';
+
+export const getPostList = createAsyncThunk('/post/list', async (_, { rejectWithValue }) => {
+  try {
+    const response = await networkInstance.get('/posts');
+    console.log(response);
+    return response.data;
+  } catch (e) {
+    rejectWithValue(e);
+  }
+});
